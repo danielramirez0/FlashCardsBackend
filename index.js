f003b3f069c94f065c3f427b35547c1308104467
Daniel Ramirez <notused@notused.net>
2021-05-05 10:17:02 -0700

    Adds CORS module for functionality

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const connectDB = require("./startup/db");
 const express = require("express");
 const app = express();
 const decks = require("./routes/decks");
-// const users = require("./routes/users");
+const cors = require("cors");
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/decks", decks);
 // app.use("/api/users", users);
